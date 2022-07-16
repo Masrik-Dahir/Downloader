822a62a61698db1dd5d362de1fd8d9848b79901c
masri <dahirma@vcu.edu>
2022-07-15 23:15:40 -0400

    dsad

--- run.sh
+++ run.sh
@@ -8,6 +8,8 @@ pip install prettytable==0.7.2
 pip install youtube-dl==2020.1.1
 pip install lxml==4.6.3
 pip install PyQt5==5.15.7
+pip install PyQt5-Qt5==5.15.2
+pip install PyQt6-sip==13.4.0
 pip install wxpython==4.1.1
 pip install pyqt5-tools==5.15.4.3.2
 pip install requests
