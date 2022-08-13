56735d0fc11f3a16a5a4bbb661b3b12e4494e355
Masrik Dahir <dahirma@vcu.edu>
2022-08-12 23:03:46 -0400

    updated
    Signed-off-by: Masrik Dahir <dahirma@vcu.edu>

--- run.sh
+++ run.sh
@@ -1,21 +1,22 @@
 pip install --upgrade pip
-pip install pytest-shutil==1.7.0
-pip install pytube==12.1.0
+pip install pytest-shutil
 pip install pytube
-pip install moviepy==1.0.3
-pip install bs4==0.0.1
-pip install prettytable==0.7.2
-pip install youtube-dl==2020.1.1
-pip install lxml==4.6.3
-pip install PyQt5==5.15.7
-pip install PyQt5-Qt5==5.15.2
-pip install PyQt5-sip==12.11.0
-pip install wxpython==4.1.1
-pip install pyqt5-tools==5.15.4.3.2
+pip install pytube
+pip install moviepy
+pip install bs4
+pip install prettytable
+pip install youtube-dl
+pip install lxml
+pip install PyQt5
+pip install PyQt5-Qt5
+pip install PyQt5-sip
+pip install wxpython
+pip install pyqt5-tools
 pip install requests
-pip install lxml==4.9.1
-pip install selenium==4.3.0
-pip install youtube_dl==2021.12.17
-pip install moviepy==1.0.3
+pip install lxml
+pip install selenium
+pip install youtube_dl
+pip install moviepy
+brew install ffmpeg
 python main.py
 
